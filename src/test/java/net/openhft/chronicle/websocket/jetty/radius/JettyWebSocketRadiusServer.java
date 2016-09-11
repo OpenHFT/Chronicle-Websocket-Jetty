1fb3bde2c054580dfed72bb663e728b243d8d35d
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2016-09-11 09:35:50 +0100

    Update copyright to Apache 2.0

--- src/test/java/net/openhft/chronicle/websocket/jetty/radius/JettyWebSocketRadiusServer.java
+++ src/test/java/net/openhft/chronicle/websocket/jetty/radius/JettyWebSocketRadiusServer.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2016 higherfrequencytrading.com
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package net.openhft.chronicle.websocket.jetty.radius;
 
 import net.openhft.chronicle.core.Jvm;
@@ -13,6 +30,10 @@ public class JettyWebSocketRadiusServer {
         this.server = new JettyWebSocketServer(port);
         server.addService("/*", Radius.class, RadiusPublisher::new);
         server.start();
+    }
+
+    public static void main(String[] args) {
+        new JettyWebSocketRadiusServer(7000);
     }
 
     static class RadiusPublisher implements IRadiusPublisher{
@@ -33,9 +54,5 @@ public class JettyWebSocketRadiusServer {
                 }
             }).start();
         }
-    }
-
-    public static void main(String[] args) {
-        new JettyWebSocketRadiusServer(7000);
     }
 }
